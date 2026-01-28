d7b8eef1d81708c9f92274c9ba07d32c07b7ab41
CristalWilsonLobo <cristal.lobo3@gmail.com>
2026-01-28 08:06:20 -0500

    Temp removing find work hyperlink

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -283,12 +283,14 @@ export default function Home() {
                 >
                   Find Talent →
                 </a>
+                {/* 
                 <a
                   href="job-seekers"
                   className="inline-flex items-center justify-center rounded-xl border border-white/25 bg-white/5 px-6 py-3 text-sm font-semibold text-white hover:bg-white/10 transition"
                 >
                   Find Work
                 </a>
+                */}
               </div>
             </div>
 
